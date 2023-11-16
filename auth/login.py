ddfcd9b755ec26f0b4083d0358fd9a14c78a0fed
ccastri <camilo.castrillon@uao.edu.co>
2023-11-16 17:41:32 -0500

    login + pdf text typing done

--- auth/login.py
+++ auth/login.py
@@ -1,86 +1,150 @@
 from fastapi import Depends, HTTPException, status, APIRouter
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.context import CryptContext
 
+
+SECRET_KEY = "c63936b20e0c03904d1c4029e73e219d715d581a4ebca46aafb1f1470ee710e5"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRES_MINS = 30
 router = APIRouter()
-
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 # Tu base de datos simulada de usuarios
-fake_users_db = {
-    "camilo@example.com": {
-        "Nombre": "CAMILO",
-        "Apellido": "CASTRILLON",
-        "Email": "camilo@example.com",
-        "Contraseña": "Bio2160cc.1607",
-        "Contraseña2": "Bio2160cc.1607",
+db = {
+    "camilo": {
+        "username": "camilo",
+        "full_name": "camilo castrillon",
+        "email": "ccastri.dev333@gmail.com",
+        "hashed_password": "$2b$12$p2oRUh41cik0Wh7PzUFzQOZNw1RNHmPRYH/cKkMRpqaiaKfiGbWGu",
+        # "Contraseña2": "Bio2160cc.1607",
     }
 }
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str or None = None
+
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 class User(BaseModel):
-    Nombre: str
-    Apellido: str
-    Email: str
+    username: str
+    full_name: str
+    email: str
 
 
 class UserInDB(User):
     hashed_password: str
 
 
-def fake_hash_password(password: str):
-    # Simulación de función para hashear la contraseña
-    return password  # En este caso, no se hace hash, se retorna la misma contraseña
-
-
-def get_user(email: str):
-    if email in fake_users_db:
-        user_dict = fake_users_db[email]
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
         return UserInDB(**user_dict)
 
 
 def verify_password(plain_password: str, hashed_password: str):
     # Comparación simple de contraseñas (en un caso real, usaríamos un método seguro como bcrypt)
-    return plain_password == hashed_password
+    return pwd_context.verify(plain_password, hashed_password)
 
 
-async def authenticate_user(email: str, contraseña: str):
-    user = get_user(email)
+def get_password_hash(password):
+    # Comparación simple de contraseñas (en un caso real, usaríamos un método seguro como bcrypt)
+    return pwd_context.hash(password)
+
+
+async def authenticate_user(db, username: str, password: str):
+    user = get_user(db, username)
     if not user:
         return False
-    if not verify_password(contraseña, user["Contraseña"]):
+    if not verify_password(password, user.hashed_password):
         return False
     return user
 
 
+async def create_access_token(data: dict, expires_delta: timedelta or None = None):
+    data_encoded = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=20)
+    data_encoded.update({"exp": expire})
+    encoded_jwt = jwt.encode(data_encoded, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
 async def get_current_user(token: str = Depends(oauth2_scheme)):
-    return fake_decode_token(token)
+    credential_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithm=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credential_exception
+        token_data = TokenData(username=username)
+
+    except JWTError:
+        raise credential_exception
+
+    user = get_user(db, username=token_data.username)
+    if user is None:
+        raise credential_exception
+
+    return user
 
 
-async def get_current_active_user(current_user: User = Depends(get_current_user)):
+async def get_current_active_user(current_user: UserInDB = Depends(get_current_user)):
     if current_user is None:
-        raise HTTPException(status_code=401, detail="Usuario no autenticado")
+        raise HTTPException(status_code=400, detail="Usuario Inactivo")
     return current_user
-
-
-def fake_decode_token(token):
-    # Simulación de decodificación de token (en una implementación real, se decodificaría el token)
-    return token
 
 
 @router.post("/token")
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = await authenticate_user(form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Credenciales inválidas",
-            headers={"WWW-Authenticate": "Bearer"},
+    try:
+        user = await authenticate_user(db, form_data.username, form_data.password)
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Credenciales inválidas",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+
+        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRES_MINS)
+        access_token = await create_access_token(
+            data={"sub": user.username}, expires_delta=access_token_expires
         )
-    # En una implementación real, aquí se generaría y se devolvería el token
-    return {"access_token": user["Email"], "token_type": "bearer"}
+
+        return {"access_token": access_token, "token_type": "bearer"}
+
+    except Exception as e:
+        # Log the error for debugging purposes
+        print(f"An error occurred during login: {e}")
+
+        # Return a generic error message to the user
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Se produjo un error durante el inicio de sesión",
+        )
 
 
 @router.get("/users/me")
 async def read_users_me(current_user: User = Depends(get_current_active_user)):
     return current_user
+
+
+pwd = get_password_hash("Bio2160cc.1607")
+print(pwd)
--- main.py
+++ main.py
@@ -59,11 +59,11 @@ app.add_middleware(
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(pdf_loader_router, prefix="/kb", tags=["kb"])
 
-gauth = GoogleAuth()
-gauth.LocalWebserverAuth()
+# gauth = GoogleAuth()
+# gauth.LocalWebserverAuth()
 
-# Assuming you have already authenticated and configured GoogleAuth
-drive = GoogleDrive(gauth)
+# # Assuming you have already authenticated and configured GoogleAuth
+# drive = GoogleDrive(gauth)
 
 
 @app.get("/")
@@ -75,49 +75,49 @@ async def main():
         return f"error: {e}"
 
 
-def list_all_files():
-    try:
-        file_list = drive.ListFile().GetList()
-        all_files = []
+# def list_all_files():
+#     try:
+#         file_list = drive.ListFile().GetList()
+#         all_files = []
 
-        for file in file_list:
-            all_files.append(
-                {
-                    "title": file["title"],
-                    "id": file["id"]
-                    # You can add more file details if needed
-                }
-            )
+#         for file in file_list:
+#             all_files.append(
+#                 {
+#                     "title": file["title"],
+#                     "id": file["id"]
+#                     # You can add more file details if needed
+#                 }
+#             )
 
-        return all_files
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error listing files: {str(e)}")
-
-
-def get_file_by_title(title: str, files_list: list):
-    for file in files_list:
-        if file["title"] == title:
-            return file
-    return None
+#         return all_files
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=f"Error listing files: {str(e)}")
 
 
-@app.get("/list-all-files")
-async def list_all_drive_files(
-    # title: str = Query(..., title="File Title")
-    title: str = "output_file",
-):
-    try:
-        all_files = list_all_files()
-        requested_file = get_file_by_title(title, all_files)
+# def get_file_by_title(title: str, files_list: list):
+#     for file in files_list:
+#         if file["title"] == title:
+#             return file
+#     return None
 
-        if requested_file:
-            return JSONResponse(content=requested_file)
-        else:
-            raise HTTPException(
-                status_code=404, detail=f"File with title '{title}' not found."
-            )
-    except HTTPException as e:
-        return JSONResponse(content={"error": e.detail}, status_code=e.status_code)
+
+# @app.get("/list-all-files")
+# async def list_all_drive_files(
+#     # title: str = Query(..., title="File Title")
+#     title: str = "output_file",
+# ):
+#     try:
+#         all_files = list_all_files()
+#         requested_file = get_file_by_title(title, all_files)
+
+#         if requested_file:
+#             return JSONResponse(content=requested_file)
+#         else:
+#             raise HTTPException(
+#                 status_code=404, detail=f"File with title '{title}' not found."
+#             )
+#     except HTTPException as e:
+#         return JSONResponse(content={"error": e.detail}, status_code=e.status_code)
 
 
 # async def list_all_drive_files():
@@ -136,130 +136,127 @@ mimetypes = {
 }
 
 
-def download_file(file_id: str, download_folder: str):
-    try:
-        file = drive.CreateFile({"id": file_id})
-        filename = file["title"]
+# def download_file(file_id: str, download_folder: str):
+#     try:
+#         file = drive.CreateFile({"id": file_id})
+#         filename = file["title"]
 
-        # Make sure the download folder exists
-        os.makedirs(download_folder, exist_ok=True)
+#         # Make sure the download folder exists
+#         os.makedirs(download_folder, exist_ok=True)
 
-        mimetypes = {
-            "application/vnd.google-apps.document": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-            "application/vnd.google-apps.spreadsheet": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-            # Add more MIME types as needed
-        }
+#         mimetypes = {
+#             "application/vnd.google-apps.document": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+#             "application/vnd.google-apps.spreadsheet": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+#             # Add more MIME types as needed
+#         }
 
-        download_mimetype = mimetypes.get(file["mimeType"], None)
+#         download_mimetype = mimetypes.get(file["mimeType"], None)
 
-        if download_mimetype:
-            file.GetContentFile(
-                os.path.join(download_folder, filename), mimetype=download_mimetype
-            )
-        else:
-            file.GetContentFile(os.path.join(download_folder, filename))
+#         if download_mimetype:
+#             file.GetContentFile(
+#                 os.path.join(download_folder, filename), mimetype=download_mimetype
+#             )
+#         else:
+#             file.GetContentFile(os.path.join(download_folder, filename))
 
-        return filename
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error downloading file: {str(e)}")
-
-
-@app.get("/download-file/{file_id}")
-async def download_drive_file(file_id: str):
-    try:
-        file_name = download_file(file_id, "/static")
-        full_path = os.path.join("/static", file_name)
-        return StreamingResponse(
-            open(full_path, "rb"),
-            media_type="application/octet-stream",
-            headers={"Content-Disposition": f"attachment;filename={file_name}"},
-        )
-    except HTTPException as e:
-        return {"error": e.detail}
+#         return filename
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=f"Error downloading file: {str(e)}")
 
 
-def access_google_drive(file_id: str = "10hVrKmMzQ-YumgQVJ3gDq_i9Fv1wdiBp1hPAO7wt2vk"):
-    try:
-        # No necesitas cargar las credenciales aquí si ya has autenticado con PyDrive
-        # Crea una instancia del servicio de Google Drive
-        drive_service = build("drive", "v3", credentials=gauth.credentials)
-
-        # Accede al archivo Excel sin descargarlo
-        response = (
-            drive_service.files()
-            .export(
-                fileId=file_id,
-                mimeType="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-            )
-            .execute()
-        )
-
-        # Procesa el contenido según tus necesidades, por ejemplo, carga en un DataFrame de pandas
-        excel_data = BytesIO(response)
-        df = pd.read_excel(excel_data)
-
-        # # Convertir columnas de tipo Timestamp a cadenas de texto
-        date_columns = [
-            "Fecha Nacimiento",
-            "Fecha Expedición",
-            "Fecha Afiliación",
-            "Fecha_Ultimo_Pago",
-        ]
-        for col in date_columns:
-            df[col] = df[col].dt.strftime("%Y-%m-%d %H:%M:%S")
-
-        # Limpiar valores flotantes para evitar errores de serialización JSON
-        df.replace([np.inf, -np.inf], np.nan, inplace=True)
-        df.fillna(0, inplace=True)  # Puedes ajustar esto según tus necesidades
-
-        # Seleccionar solo las columnas de interés
-        columns_of_interest = [
-            "Tipo Identificacion",
-            "Identificación",
-            "Primer Nombre",
-            "Segundo Nombre",
-            "Primer Apellido",
-            "Segundo Apellido",
-            "Numero Celular",
-            "Email",
-            "Link_Planillas",
-            "Pago_Realizado",
-            "Fecha_Ultimo_Pago",
-        ]
-
-        df_subset = df[(df["Pago_Realizado"] == "SI") & (df["Fecha_Ultimo_Pago"] != 0)]
-
-        # Iterar sobre cada fila del subconjunto filtrado y enviar el correo a cada usuario
-        # for index, user in df_subset.iterrows():
-        #     # Enviar el enlace del archivo por WhatsApp
-        #     enviar_pdf_desde_google_drive(
-        #         f"Link de la planilla: {user['Link_Planillas']}",
-        #         # user["Numero Celular"],
-        #         # user["Primer Nombre"],
-        #         user[
-        #             "Email"
-        #         ],  # Asumiendo que 'Numero Celular' contiene el número de WhatsApp
-        #     )
-
-        # [enviar_pdf_desde_google_drive(
-        #     user["Primer Nombre"], user["Email"]
-        # ) for user in df['subset']]
-        return df_subset
-    except Exception as e:
-        raise HTTPException(
-            status_code=500, detail=f"Error accessing Google Drive: {str(e)}"
-        )
+# @app.get("/download-file/{file_id}")
+# async def download_drive_file(file_id: str):
+#     try:
+#         file_name = download_file(file_id, "/static")
+#         full_path = os.path.join("/static", file_name)
+#         return StreamingResponse(
+#             open(full_path, "rb"),
+#             media_type="application/octet-stream",
+#             headers={"Content-Disposition": f"attachment;filename={file_name}"},
+#         )
+#     except HTTPException as e:
+#         return {"error": e.detail}
 
 
-@app.get("/access-google-drive/{file_id}")
-async def access_google_drive_endpoint(
-    file_id: str = "10hVrKmMzQ-YumgQVJ3gDq_i9Fv1wdiBp1hPAO7wt2vk",
-):
-    try:
-        result_df = access_google_drive(file_id)
-        return JSONResponse(content=result_df.to_dict(orient="records"))
-    except HTTPException as e:
-        return JSONResponse(content={"error": e.detail}, status_code=e.status_code)
+# def access_google_drive(file_id: str = "10hVrKmMzQ-YumgQVJ3gDq_i9Fv1wdiBp1hPAO7wt2vk"):
+#     try:
+#         # No necesitas cargar las credenciales aquí si ya has autenticado con PyDrive
+#         # Crea una instancia del servicio de Google Drive
+#         drive_service = build("drive", "v3", credentials=gauth.credentials)
+
+#         # Accede al archivo Excel sin descargarlo
+#         response = (
+#             drive_service.files()
+#             .export(
+#                 fileId=file_id,
+#                 mimeType="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+#             )
+#             .execute()
+#         )
+
+#         # Procesa el contenido según tus necesidades, por ejemplo, carga en un DataFrame de pandas
+#         excel_data = BytesIO(response)
+#         df = pd.read_excel(excel_data)
+
+#         # # Convertir columnas de tipo Timestamp a cadenas de texto
+#         date_columns = [
+#             "Fecha Nacimiento",
+#             "Fecha Expedición",
+#             "Fecha Afiliación",
+#             "Fecha_Ultimo_Pago",
+#         ]
+#         for col in date_columns:
+#             df[col] = df[col].dt.strftime("%Y-%m-%d %H:%M:%S")
+
+#         # Limpiar valores flotantes para evitar errores de serialización JSON
+#         df.replace([np.inf, -np.inf], np.nan, inplace=True)
+#         df.fillna(0, inplace=True)  # Puedes ajustar esto según tus necesidades
+
+#         # Seleccionar solo las columnas de interés
+#         columns_of_interest = [
+#             "Tipo Identificacion",
+#             "Identificación",
+#             "Primer Nombre",
+#             "Segundo Nombre",
+#             "Primer Apellido",
+#             "Segundo Apellido",
+#             "Numero Celular",
+#             "Email",
+#             "Link_Planillas",
+#             "Pago_Realizado",
+#             "Fecha_Ultimo_Pago",
+#         ]
+
+#         df_subset = df[(df["Pago_Realizado"] == "SI") & (df["Fecha_Ultimo_Pago"] != 0)]
+
+#         # Iterar sobre cada fila del subconjunto filtrado y enviar el correo a cada usuario
+#         # for index, user in df_subset.iterrows():
+#         #     # Enviar el enlace del archivo por WhatsApp
+#         #     enviar_pdf_desde_google_drive(
+#         #         f"Link de la planilla: {user['Link_Planillas']}",
+#         #         # user["Numero Celular"],
+#         #         # user["Primer Nombre"],
+#         #         user[
+#         #             "Email"
+#         #         ],  # Asumiendo que 'Numero Celular' contiene el número de WhatsApp
+#         #     )
+
+#         return df_subset
+#     except Exception as e:
+#         raise HTTPException(
+#             status_code=500, detail=f"Error accessing Google Drive: {str(e)}"
+#         )
+
+
+# @app.get("/access-google-drive/{file_id}")
+# async def access_google_drive_endpoint(
+#     file_id: str = "10hVrKmMzQ-YumgQVJ3gDq_i9Fv1wdiBp1hPAO7wt2vk",
+# ):
+#     try:
+#         result_df = access_google_drive(file_id)
+#         return JSONResponse(content=result_df.to_dict(orient="records"))
+#     except HTTPException as e:
+#         return JSONResponse(content={"error": e.detail}, status_code=e.status_code)
 
 
 if __name__ == "__main__":
@@ -269,80 +266,59 @@ if __name__ == "__main__":
 
 
 # Función para descargar y enviar un archivo PDF desde Google Drive por correo electrónico
-def enviar_pdf_desde_google_drive(
-    file_link: str,
-    #   name_sender: str,
-    email: str,
-    provider: str = "gmail",
-):
-    # Configurar la autenticación de PyDrive
-    # Obtener el ID del archivo desde el enlace de Google Drive
-    # file_id = file_link.split("/")[-2]
-    # filename = f"{name_sender}.pdf"
+# def enviar_pdf_desde_google_drive(
+#     file_link: str,
+#     #   name_sender: str,
+#     email: str,
+#     provider: str = "gmail",
+# ):
+#     # Configurar la autenticación de PyDrive
+#     # Obtener el ID del archivo desde el enlace de Google Drive
+#     # file_id = file_link.split("/")[-2]
+#     # filename = f"{name_sender}.pdf"
 
-    gauth = GoogleAuth()
-    gauth.LocalWebserverAuth()
+#     gauth = GoogleAuth()
+#     gauth.LocalWebserverAuth()
 
-    # Autenticar con Google Drive
-    drive = GoogleDrive(gauth)
+#     # Autenticar con Google Drive
+#     drive = GoogleDrive(gauth)
 
-    try:
-        # file = drive.CreateFile({"id": file_id})
-        # file.GetContentFile(filename)  # Descargar el archivo PDF
-
-        # Configurar los parámetros para enviar el correo electrónico
-        from_address = "cycaccionlegalsas@gmail.com"
-        password = "kvbs wfum qrst bvsx"
-        to_address = email
-
-        msg = MIMEMultipart()
-        msg["From"] = from_address
-        msg["To"] = to_address
-        msg[
-            "Subject"
-        ] = "Envio de planilla automatizado desde Google Drive. Bechos gordito"
-
-        # Configurar el servidor SMTP y envío del correo electrónico según el proveedor
-        if provider.lower() == "gmail":
-            smtp_server = "smtp.gmail.com"
-            port = 587
-        elif provider.lower() == "hotmail":
-            smtp_server = "smtp.live.com"  # Puedes usar "smtp.office365.com" si es una cuenta de Office 365
-            port = 587
-        else:
-            raise ValueError("Proveedor de correo no compatible")
-        # Adjuntar el archivo PDF al correo electrónico       # Cuerpo del correo con el enlace del archivo de Google Drive
-        body = f"Estimado/a,\n\nAquí está el enlace al archivo: {file_link}\n\nAtentamente,\nTu empresa"
-        msg.attach(MIMEText(body, "plain"))
-        # Configuración del servidor SMTP y envío del correo electrónico
-        server = smtplib.SMTP("smtp.gmail.com", 587)
-        server.starttls()
-        server.login(from_address, password)
-        server.send_message(msg)
-        server.quit()
-
-        print("Correo electrónico enviado con el archivo adjunto.")
-
-    except Exception as e:
-        print(f"Error: {str(e)}")
-
-
-# Utilizar la función para enviar el archivo PDF a un destinatario específico
-# Reemplaza 'tu_enlace_de_google_drive' con el enlace proporcionado en 'Link_Planillas'
-# Reemplaza 'destinatario@example.com' con la dirección de correo del destinatario
-# enviar_pdf_desde_google_drive("tu_enlace_de_google_drive", "destinatario@example.com")
-# def enviar_whatsapp(mensaje: str, numero_whatsapp: str):
-#     # Tu cuenta SID y token de autenticación de Twilio
-#     token = "AC2118693af16ced6cc925ef4497555481"
-#     id_phone_number = "de4d84b1d856f79bb62cdde6b3a21a95"
-#     # sender_phone = Client(account_sid, auth_token)
-
-#     # Número de WhatsApp de origen (obtenido de Twilio)
-#     sender_phone = "+573107282535"  # Cambia esto con tu número de Twilio
-#     message = "Teesting testing"
 #     try:
-#         message_whatssapp = WhatsApp(token, id_phone_number)
-#         message_whatssapp.send_message(message, sender_phone)
-#         print(f"Mensaje enviado a {numero_whatsapp}")
+#         # file = drive.CreateFile({"id": file_id})
+#         # file.GetContentFile(filename)  # Descargar el archivo PDF
+
+#         # Configurar los parámetros para enviar el correo electrónico
+#         from_address = "cycaccionlegalsas@gmail.com"
+#         password = "kvbs wfum qrst bvsx"
+#         to_address = email
+
+#         msg = MIMEMultipart()
+#         msg["From"] = from_address
+#         msg["To"] = to_address
+#         msg[
+#             "Subject"
+#         ] = "Envio de planilla automatizado desde Google Drive. Bechos gordito"
+
+#         # Configurar el servidor SMTP y envío del correo electrónico según el proveedor
+#         if provider.lower() == "gmail":
+#             smtp_server = "smtp.gmail.com"
+#             port = 587
+#         elif provider.lower() == "hotmail":
+#             smtp_server = "smtp.live.com"  # Puedes usar "smtp.office365.com" si es una cuenta de Office 365
+#             port = 587
+#         else:
+#             raise ValueError("Proveedor de correo no compatible")
+#         # Adjuntar el archivo PDF al correo electrónico       # Cuerpo del correo con el enlace del archivo de Google Drive
+#         body = f"Estimado/a,\n\nAquí está el enlace al archivo: {file_link}\n\nAtentamente,\nTu empresa"
+#         msg.attach(MIMEText(body, "plain"))
+#         # Configuración del servidor SMTP y envío del correo electrónico
+#         server = smtplib.SMTP("smtp.gmail.com", 587)
+#         server.starttls()
+#         server.login(from_address, password)
+#         server.send_message(msg)
+#         server.quit()
+
+#         print("Correo electrónico enviado con el archivo adjunto.")
+
 #     except Exception as e:
-#         print(f"Error al enviar el mensaje: {str(e)}")
+#         print(f"Error: {str(e)}")
